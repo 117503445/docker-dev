8444283acba0c7acb12d92020c04390fae7e0b50
117503445 <t117503445@gmail.com>
2023-07-25 22:11:47 +0800

    rm code install ext
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- public/vsc_init.sh
+++ public/vsc_init.sh
@@ -1,2 +1,3 @@
+# TODO: install extension
 code --install-extension mhutchie.git-graph christian-kohler.path-intellisense esbenp.prettier-vscode humao.rest-client uctakeoff.vscode-counter redhat.vscode-yaml mechatroner.rainbow-csv 2gua.rainbow-brackets
 # /usr/local/bin/code --install-extension mhutchie.git-graph christian-kohler.path-intellisense esbenp.prettier-vscode humao.rest-client uctakeoff.vscode-counter redhat.vscode-yaml mechatroner.rainbow-csv 2gua.rainbow-brackets
