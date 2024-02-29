f5bdfba874a21345bbe4d5b929f867113f9c8669
117503445 <t117503445@gmail.com>
2024-02-29 14:55:58 +0800

    1
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- scripts/arch_init.sh
+++ scripts/arch_init.sh
@@ -22,6 +22,16 @@ LANG=en_US.utf-8
 LC_ALL=en_US.utf-8
 EOF
 
+# https://preciselab.io/how-to-install-yay-on-pure-archlinux-image/
+pacman -Sy base-devel --noconfirm
+mkdir -p /tmp/yay-build
+useradd -m -G wheel builder && passwd -d builder
+chown -R builder:builder /tmp/yay-build
+echo 'builder ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+su - builder -c "git clone https://aur.archlinux.org/yay.git /tmp/yay-build/yay"
+su - builder -c "cd /tmp/yay-build/yay && makepkg -si --noconfirm"
+rm -rf /tmp/yay-build
+
 mkdir -p ~/.config/fish
 cat << EOF > ~/.config/fish/config.fish
 if status is-interactive
