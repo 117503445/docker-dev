ed32bfcfa01d1374650e5551c0f1bca412a7cc3c
117503445 <t117503445@gmail.com>
2024-04-07 21:05:52 +0800

    1
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+echo "=== Install VSC Server & Extensions ==="
 curl -s https://vsc-server.unidrop.top/vsc_server_setup.sh | sh
+echo "=== Install VSC Server & Extensions Done ==="
 
+echo "=== Update Arch Linux Packages ==="
 pacman -Syu --noconfirm
+echo "=== Update Arch Linux Packages Done ==="
+
+echo "~~~ Successfully Init Development Environment, Enjoy! ~~~"
 
 # check if stdin is a terminal
 if [ -t 0 ]; then
