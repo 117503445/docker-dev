3f1562e7a79d7df664828d5982a417adddf27078
117503445 <t117503445@gmail.com>
2024-06-09 11:24:42 +0800

    1
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- entrypoint/go.mod
+++ entrypoint/go.mod
@@ -1,10 +1,14 @@
 module github.com/117503445/docker-dev/entrypoint
 
-go 1.22.3
+go 1.22.4
+
+require (
+	github.com/117503445/goutils v0.0.0-20240608174601-111440f8d7f8
+	github.com/mattn/go-isatty v0.0.20
+	github.com/rs/zerolog v1.33.0
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 )
--- entrypoint/main.go
+++ entrypoint/main.go
@@ -6,16 +6,16 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/rs/zerolog"
+	// "github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"os/exec"
 
 	"github.com/mattn/go-isatty"
+	"github.com/117503445/goutils"
 )
 
 func installVscExtensions() {
-
 	// list all dirs in /root/.vscode-server/bin
 	vscVersions, err := filepath.Glob("/root/.vscode-server/bin/*")
 	if err != nil {
@@ -33,26 +33,26 @@ func installVscExtensions() {
 	exts := strings.Split(vscExts, ",")
 	for _, ext := range exts {
 		log.Debug().Str("ext", ext).Msg("Install Visual Studio Code Extension")
-		CMD("", fileVsc, "--install-extension", ext)
+		goutils.CMD("", fileVsc, "--install-extension", ext)
 		log.Debug().Str("ext", ext).Msg("Install Visual Studio Code Extension Done")
 	}
 
-	CMD("", fileVsc, "--update-extensions")
+	goutils.CMD("", fileVsc, "--update-extensions")
 }
 
 func main() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "2006-01-02 15:04:05"})
+	goutils.InitZeroLog()
 
 	installVscExtensions()
 
 	log.Debug().Msg("Update Arch Linux Packages")
-	CMD("", "pacman", "-Syu", "--noconfirm")
+	goutils.CMD("", "pacman", "-Syu", "--noconfirm")
 	log.Debug().Msg("Update Arch Linux Packages Done")
 
 	fileCustomEntrypoint := "/entrypoint"
 	if _, err := os.Stat(fileCustomEntrypoint); err == nil {
 		log.Debug().Msg("Run Custom Entrypoint")
-		CMD("", fileCustomEntrypoint)
+		goutils.CMD("", fileCustomEntrypoint)
 		log.Debug().Msg("Run Custom Entrypoint Done")
 	}
 
@@ -79,31 +79,6 @@ func main() {
 		}
 		log.Debug().Msg("Exit fish shell")
 	} else {
-		CMD("", "tail", "-f", "/dev/null")
-	}
-}
-
-func CMD(cwd string, command string, args ...string) {
-	var err error
-	if cwd == "" {
-		cwd, err = os.Getwd()
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to get current working directory")
-			return
-		}
-	}
-	commandStr := command
-	for _, arg := range args {
-		commandStr += " " + arg
-	}
-	log.Debug().Str("cwd", cwd).Str("command", commandStr).Msg("Run Command")
-	cmd := exec.Command(command, args...)
-	cmd.Dir = cwd
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdout
-	err = cmd.Run()
-	if err != nil {
-		log.Error().Err(err).Str("cwd", cwd).Str("command", command).Strs("args", args).
-			Msg("Failed to run command")
+		goutils.CMD("", "tail", "-f", "/dev/null")
 	}
 }
